a419d53c672627edd09a1be140424aff71aa4f1f
Shamshad Ansari <149064103+shamshad-ansari@users.noreply.github.com>
2025-05-29 15:45:01 -0500

    Updated userSchema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  goal: String,
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  goal: String,  
 });
 
 module.exports = mongoose.model("User", userSchema);
